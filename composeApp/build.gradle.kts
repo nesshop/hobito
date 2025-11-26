59a20affb4d317ddd805d28a6c2d2a1e40df8962
Ernesto <ernesto.muros@vass.es>
2025-11-26 21:08:02 +0100

    🔧 Add cocoapods dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.kotlinCocoapods)
 }
 
 kotlin {
